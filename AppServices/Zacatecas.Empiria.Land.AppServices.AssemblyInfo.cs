26a0a2c1bbb6af9b7a85fab88a18f6fa281b611e
José Manuel Cota <jmcota@ontica.org>
2020-11-23 21:28:46 -0500

    Application services module added (as a web api aggregator)

---
+++ AppServices/Zacatecas.Empiria.Land.AppServices.AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿/* Empiria Land **********************************************************************************************
+*                                                                                                            *
+*  Module   : Zacatecas Empiria Land App Services        Component : Application services                    *
+*  Assembly : Zacatecas.Empiria.Land.AppServices.dll     Pattern   : Assembly Attributes File                *
+*  Type     : None                                       License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Contiene casos de uso especiales para el Registro Público del Estado de Zacatecas y sirve como *
+*             aglutinador de servicios web de tipo Http/Json de la solución.                                 *
+*                                                                                                            *
+*             A través de sus componentes, es posible modificar el comportamiento predeterminado de las web  *
+*             apis, de los componentes y de los casos de uso que conforman Empiria Land.                     *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+using System.Reflection;
+
+/*************************************************************************************************************
+* Assembly configuration attributes.                                                                         *
+*************************************************************************************************************/
+[assembly: AssemblyTrademark("Empiria and Ontica are either trademarks of La Vía Óntica SC or Ontica LLC.")]
+[assembly: AssemblyCulture("")]
+[assembly: CLSCompliant(true)]
