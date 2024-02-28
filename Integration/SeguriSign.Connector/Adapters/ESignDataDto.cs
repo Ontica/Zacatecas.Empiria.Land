7463843689db32065beb534b0ea40c82c39bd3cc
José Manuel Cota <jmcota@ontica.org>
2024-02-28 10:37:27 -0600

    Extract the remaining Xml data and add them to ESignDataDto

--- Integration/SeguriSign.Connector/Adapters/ESignDataDto.cs
+++ Integration/SeguriSign.Connector/Adapters/ESignDataDto.cs
@@ -8,14 +8,67 @@
 *                                                                                                            *
 ************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
 
+using System;
+
 namespace SeguriSign.Connector.Adapters {
 
   /// <summary>Output DTO with electronic sign data.</summary>
   public class ESignDataDto {
 
+    public string DocumentID {
+      get; internal set;
+    }
+
+    public string DocumentName {
+      get; internal set;
+    }
+
+    public string AuthorityName {
+      get; internal set;
+    }
+
+    public string SerialNumber {
+      get; internal set;
+    }
+
+    public string SerialName {
+      get; internal set;
+    }
+
+    public DateTime SignDate {
+      get; internal set;
+    }
+
+    public DateTime LocalSignDate {
+      get; internal set;
+    }
+
+    public string SignatoryName {
+      get; internal set;
+    }
+
+    public string SignatoryRole {
+      get; internal set;
+    }
+
+    public string SignatureAlgorithm {
+      get; internal set;
+    }
+
     public string Signature {
-      get;
-      internal set;
+      get; internal set;
+    }
+
+    public string Digest {
+      get; internal set;
+    }
+
+    public string ResponderIssuer {
+      get; internal set;
+    }
+
+    public string ResponderName {
+      get; internal set;
     }
 
   }  // class ESignDataDto
--- Integration/SeguriSign.Connector/Adapters/ESignXmlToSignDataConverter.cs
+++ Integration/SeguriSign.Connector/Adapters/ESignXmlToSignDataConverter.cs
@@ -25,18 +25,137 @@ namespace SeguriSign.Connector.Adapters {
 
     internal ESignDataDto Convert() {
       return new ESignDataDto {
-        Signature = GetESign()
+        DocumentID = GetDocumentID(),
+        DocumentName = GetDocumentName(),
+        AuthorityName = GetAuthorityName(),
+        SerialNumber = GetSerialNumber(),
+        SerialName = GetSerialName(),
+        SignDate = GetSignDate(),
+        LocalSignDate = GetLocalSignDate(),
+        SignatoryName = GetSignatoryName(),
+        SignatoryRole = GetSignatoryRole(),
+        SignatureAlgorithm = GetSignatureAlgorithm(),
+        Signature = GetSignature(),
+        Digest = GetDigest(),
+        ResponderIssuer = GetResponderIssuer(),
+        ResponderName = GetResponderName()
       };
     }
 
     #region Helpers
 
-    private string GetESign() {
+    private string GetAuthorityName() {
+      var xpath = "/signinginfo/ca";
+
+      XmlNode node = _xml.SelectSingleNode(xpath);
+
+      return node.Attributes["name"].Value;
+    }
+
+    private string GetDigest() {
+      var xpath = "/signinginfo/ca/signatory/tsp";
+
+      XmlNode node = _xml.SelectSingleNode(xpath);
+
+      return node.Attributes["digest"].Value;
+    }
+
+    private string GetDocumentID() {
+      var xpath = "/signinginfo/document";
+
+      XmlNode node = _xml.SelectSingleNode(xpath);
+
+      return node.Attributes["id"].Value;
+    }
+
+    private string GetDocumentName() {
+      var xpath = "/signinginfo/document";
+
+      XmlNode node = _xml.SelectSingleNode(xpath);
+
+      return node.Attributes["name"].Value;
+    }
+
+    private DateTime GetLocalSignDate() {
+      var xpath = "/signinginfo/ca/signatory/signature";
+
+      XmlNode node = _xml.SelectSingleNode(xpath);
+
+      return DateTime.Parse(node.Attributes["localdate"].Value);
+    }
+
+    private string GetResponderIssuer() {
+      var xpath = "/signinginfo/ca/signatory/tsp";
+
+      XmlNode node = _xml.SelectSingleNode(xpath);
+
+      return node.Attributes["responderissuer"].Value;
+    }
+
+    private string GetResponderName() {
+      var xpath = "/signinginfo/ca/signatory/tsp";
+
+      XmlNode node = _xml.SelectSingleNode(xpath);
+
+      return node.Attributes["respondername"].Value;
+    }
+
+    private string GetSerialNumber() {
+      var xpath = "/signinginfo/ca/signatory";
+
+      XmlNode node = _xml.SelectSingleNode(xpath);
+
+      return node.Attributes["serialnum"].Value;
+    }
+
+    private string GetSerialName() {
+      var xpath = "/signinginfo/ca/signatory";
+
+      XmlNode node = _xml.SelectSingleNode(xpath);
+
+      return node.Attributes["serialname"].Value;
+    }
+
+
+    private string GetSignatoryName() {
+      var xpath = "/signinginfo/ca/signatory";
+
+      XmlNode node = _xml.SelectSingleNode(xpath);
+
+      return node.Attributes["name"].Value;
+    }
+
+    private string GetSignatoryRole() {
+      var xpath = "/signinginfo/ca/signatory";
+
+      XmlNode node = _xml.SelectSingleNode(xpath);
+
+      return node.Attributes["role"].Value;
+    }
+
+    private string GetSignature() {
       var xpath = "/signinginfo/ca/signatory/signature";
 
       XmlNode node = _xml.SelectSingleNode(xpath);
 
       return node.InnerText;
+    }
+
+    private string GetSignatureAlgorithm() {
+      var xpath = "/signinginfo/ca/signatory/signature";
+
+      XmlNode node = _xml.SelectSingleNode(xpath);
+
+      return node.Attributes["algorithm"].Value;
+    }
+
+    private DateTime GetSignDate() {
+      var xpath = "/signinginfo/ca/signatory/signature";
+
+      XmlNode node = _xml.SelectSingleNode(xpath);
+
+      return DateTime.Parse(node.Attributes["date"].Value)
+                     .ToUniversalTime();
     }
 
     #endregion Helpers
