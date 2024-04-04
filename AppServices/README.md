b5c52b04aea11d9face179553bde47d3d35415f2
José Manuel Cota <jmcota@ontica.org>
2024-04-04 08:42:56 -0400

    Update project references

--- AppServices/README.md
+++ AppServices/README.md
@@ -1,7 +1,7 @@
 ﻿# Zacatecas Empiria Land Application Services
 
 Contiene casos de uso especiales para el Registro Público del Estado de Zacatecas
-y sirve como aglutinador de servicios web de tipo Http/Json de la solución.
+y sirve como agrupador de servicios web de tipo Http/Json de la solución.
 
 A través de sus componentes, es posible modificar el comportamiento predeterminado
 de las web apis, de los componentes y de los casos de uso que conforman Empiria Land.
--- AppServices/Zacatecas.Empiria.Land.AppServices.AssemblyInfo.cs
+++ AppServices/Zacatecas.Empiria.Land.AppServices.AssemblyInfo.cs
@@ -5,7 +5,7 @@
 *  Type     : None                                       License   : Please read LICENSE.txt file            *
 *                                                                                                            *
 *  Summary  : Contiene casos de uso especiales para el Registro Público del Estado de Zacatecas y sirve como *
-*             aglutinador de servicios web de tipo Http/Json de la solución.                                 *
+*             agrupador de servicios web de tipo Http/Json de la solución.                                   *
 *                                                                                                            *
 *             A través de sus componentes, es posible modificar el comportamiento predeterminado de las web  *
 *             apis, de los componentes y de los casos de uso que conforman Empiria Land.                     *
